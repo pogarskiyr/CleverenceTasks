de36e8722c3157a2e87862a9936122978452ebdc
pogarskiyr <pogarskiyr@gmail.com>
2026-08-03 23:21:37 +0300

    Add files via upload

---
+++ task2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+static class Server
+{
+    private static int count = 0;
+    private static readonly ReaderWriterLockSlim _servLock = new ReaderWriterLockSlim();
+    public static int GetCount()
+    {
+        _servLock.EnterReadLock();
+
+        try
+        {
+            return count;
+        }
+        finally
+        {
+            _servLock.ExitReadLock();
+        }
+    }
+    public static void AddToCount(int a)
+    {
+        _servLock.EnterWriteLock();
+
+        try
+        {
+            count = checked(count + a);
+        }
+        finally
+        {
+            _servLock.ExitWriteLock();
+        }
+    }
+}
+
+internal class Program
+{
+    static async void TestServer()
+    {
+        var rTasks = new Task[10];
+        for (int i = 0; i < rTasks.Length; i++)
+            rTasks[i] = Task.Run(() => Server.GetCount());
+        await Task.WhenAll(rTasks);
+
+        return;
+    }
+    static void Main()
+    {
+        
+
+    }
+}
